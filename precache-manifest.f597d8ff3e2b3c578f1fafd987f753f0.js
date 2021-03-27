f690754689c5bc4d97ff7f5e6e4bdd7e63333c66
Freedy <21914103@zju.edu.cn>
2021-03-27 10:36:31 +0800

    deploy

---
+++ precache-manifest.f597d8ff3e2b3c578f1fafd987f753f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d39fcdf9061cf805a6",
+    "url": "/charge-account-v2-websit/css/app.468befd3.css"
+  },
+  {
+    "revision": "f2f85a0eb1878335f811eadaee3eec22",
+    "url": "/charge-account-v2-websit/index.html"
+  },
+  {
+    "revision": "36d39fcdf9061cf805a6",
+    "url": "/charge-account-v2-websit/js/app.b2417cdd.js"
+  },
+  {
+    "revision": "ceca1c567f71a640fd3e",
+    "url": "/charge-account-v2-websit/js/chunk-vendors.f33708d1.js"
+  },
+  {
+    "revision": "2f1976078af56fe04d6487277f202ac6",
+    "url": "/charge-account-v2-websit/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/charge-account-v2-websit/robots.txt"
+  }
+]);
